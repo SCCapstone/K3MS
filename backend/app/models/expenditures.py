d7cd77bc4d480ec11d44a5ce96f652c396ea6e14
MMAzeem3 <musa.mazeem@gmail.com>
2023-11-30 17:42:58 -0500

    expenditures: renamed year row for consistency and  made it primary key, grants: added year column and make title PK, publications: no change - fixed indentation for all

--- backend/app/models/expenditures.py
+++ backend/app/models/expenditures.py
@@ -5,8 +5,8 @@ from datetime import datetime
 @dataclass
 class Expenditures(db.Model):
     email: str = db.Column(db.String(100), nullable=False, primary_key=True)
-    calendar_year: str = db.Column(db.String(100), nullable=False)
-	reporting_department: str = db.Column(db.String(100), nullable=False)
-	pi_name: str = db.Column(db.String(100), nullable=False)
-	amount: int = db.Column(db.Integer, nullable=False)
-	date_added: str = db.Column(db.DateTime, default=datetime.utcnow)
+    year: str = db.Column(db.String(100), nullable=False, primary_key=True)
+    reporting_department: str = db.Column(db.String(100), nullable=False)
+    pi_name: str = db.Column(db.String(100), nullable=False)
+    amount: int = db.Column(db.Integer, nullable=False)
+    date_added: str = db.Column(db.DateTime, default=datetime.utcnow)
--- backend/app/models/grants.py
+++ backend/app/models/grants.py
@@ -5,6 +5,7 @@ from datetime import datetime
 @dataclass
 class Grants(db.Model):
     email: str = db.Column(db.String(100), nullable=False, primary_key=True)
-    title: str = db.Column(db.String(500), nullable=False)
-	amount: int = db.Column(db.Integer, nullable=False)
-	date_added: str = db.Column(db.DateTime, default=datetime.utcnow)
+    title: str = db.Column(db.String(500), nullable=False, primary_key=True)
+    year: str = db.Column(db.String(100), nullable=False)
+    amount: int = db.Column(db.Integer, nullable=False)
+    date_added: str = db.Column(db.DateTime, default=datetime.utcnow)
