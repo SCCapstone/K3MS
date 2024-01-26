1c498cbd4149c0d974a15c6e0ab84e29dcc493b9
SavannahNoblitt <savannahmanob@gmail.com>
2024-01-25 20:53:52 -0500

    Backend Functionality for Updating Passwords

--- backend/app/account_settings_module/controller.py
+++ backend/app/account_settings_module/controller.py
@@ -1,16 +1,61 @@
 from http import HTTPStatus
-from app.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from app.extensions import db
-from flask_login import login_user, logout_user, current_user
-from flask import jsonify
+from flask_login import current_user
 
-update_password_fields = [    
-    'old_password',
+update_password_fields = [
     'new_password',
-    'confirm_new-password'
+    'confirm_new_password'
 ]
 
 def update_password_controller(req):
 
-    return None
+    # Validate Request
+    ret = validate_request(req, update_password_fields)
+
+    if isinstance(ret, tuple):
+        return ret
+    
+    json_data = ret
+
+    # Get Update Password Fields
+    new_password = json_data.get(update_password_fields[0])
+    confirm_new_password = json_data.get(update_password_fields[1])
+
+    # Error If new_password and confirm_new_password Aren't The Same
+    if confirm_new_password != new_password:
+        return dict(error='New Password Cannot Be Confirmed'), HTTPStatus.BAD_REQUEST
+    
+    # Reset current_user's Password In The DB
+    current_user.password_hash = generate_password_hash(new_password, method='scrypt')
+
+    # Commit Changes To The DB
+    db.session.commit()
+
+    return dict(mssg='Password Updated Successfully!'), HTTPStatus.OK
+
+def validate_request(req, fields):
+    # Make sure request is JSON
+    content_type = req.headers.get('Content-Type')
+    if content_type != 'application/json':
+        return dict(error='Content-Type not supported'), HTTPStatus.BAD_REQUEST
+
+    # Make sure request has JSON data
+    json_data = req.get_json()
+    if not json_data:
+        return dict(error='Missing JSON data'), HTTPStatus.BAD_REQUEST
+
+    # Make sure all fields are filled in
+    empty_fields = [] # track any missing fields
+    for field in fields:
+        if not json_data.get(field):
+            empty_fields.append(field)
+    
+    # If any fields are missing, return error
+    if len(empty_fields) > 0:
+        return dict(
+            error='Please fill in all fields',
+            empty_fields=empty_fields
+        ), HTTPStatus.BAD_REQUEST
+
+    return json_data
